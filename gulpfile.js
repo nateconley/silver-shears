476aabcea395730ca54b9011fcb973f6d2da6d52
Nathan Conley <nateconley123@gmail.com>
2016-06-21 20:45:14 -0700

    Boilerplate and very basic header styling

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,12 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const browserSync = require('browser-sync');
 
-gulp.task('serve', function(){
+gulp.task('serve', ['sass'], function(){
 
 	browserSync.init({
+		open: false,
 		server: {
-			baseDir: './',
-			open: false
+			baseDir: './'
 		}
 	});
 
@@ -22,7 +22,7 @@ gulp.task('sass', function(){
 	return gulp.src('./styles.sass')
 		.pipe(sass())
 		.pipe(gulp.dest('./'))
-		.pipe(browserSync.stream);
+		.pipe(browserSync.stream());
 });
 
 gulp.task('default', ['serve']);
